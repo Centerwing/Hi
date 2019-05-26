6ac2fb261e2cab08d8543915c8f273c8009891b9
冬阳 刘 <392702533@qq.com>
2019-05-26 20:27:57 +0800

    添加项目文件。

---
+++ MusicController.cs
@@ -0,0 +1,75 @@
+﻿namespace MusicController
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Media;
+    using System.Reflection;
+    using System.Threading;
+    using Microsoft.Kinect;
+    using Microsoft.Kinect.VisualGestureBuilder;
+    using Newtonsoft.Json;
+    class MusicController
+    {
+        Dictionary<string,InstrumentBase> instruments;
+        //
+        private DateTime lastRing;
+        //
+        public MusicController(string[] discreteGestureNames, string[] conGestureNames = null)
+        {
+            //gesture names
+            this.DiscreteGestureNames = discreteGestureNames;
+            this.ConGestureNames = conGestureNames;
+            InitInstruments();
+        }
+        public void InitInstruments()
+        {
+            string text = System.IO.File.OpenText("config.json").ReadToEnd();
+
+            var dic = JsonConvert.DeserializeObject<Dictionary<string, InstrumentConfig>>(text);
+
+            //add all instruments;
+            var config = new InstrumentConfig();
+            {
+                foreach (var gestureName in DiscreteGestureNames)
+                {
+                    dic.TryGetValue(gestureName, out config);
+                    if(config!=null)
+                    {
+                        if (config.isDiscrete)
+                        {
+                            instruments.Add(gestureName, new InstrumentDis(gestureName, config));
+                        }
+                        else
+                        {
+                            instruments.Add(gestureName, new InstrumentCon(gestureName, config));
+                        }
+                    }
+                }
+            }
+
+        }
+
+        public void ProcessGestureResults(IReadOnlyDictionary<Gesture, DiscreteGestureResult> discreteResults)
+        {
+            foreach (var gestureResult in discreteResults)
+            {
+                instruments[gestureResult.Key.Name].Play(gestureResult.Value.Confidence);
+
+            }
+        }
+        //npName===namespaceName
+        public string NpName
+        {
+            get;
+            private set;
+        }
+        //
+        public string[] DiscreteGestureNames { get; private set; }
+        public string[] ConGestureNames { get; private set; }
+       
+    }
+
+}
---
+++ instruments/InstrumentBase.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using System.Reflection;
+namespace MusicController
+{
+    abstract class InstrumentBase
+    {
+        public InstrumentBase(string insName, InstrumentConfig config)
+        {
+            //这里必须是主程序（Microsoft.Samples.Kinect.DiscreteGestureBasics）的namespace
+            string npName = Assembly.GetExecutingAssembly().GetName().Name.ToString();
+            string musicPath = @".music";
+            var assembly = Assembly.GetExecutingAssembly();
+            System.IO.Stream stream = assembly.GetManifestResourceStream(npName + musicPath + ".guitar.wav");
+            sp = new SoundPlayer(stream);
+            threshold = config.threshold;
+            delay = config.delay;
+        }
+        public abstract void Play(double confidience);
+        ///returns the soundPlayer
+        protected SoundPlayer sp { get; set; }
+        protected DateTime lastDetection;
+        protected double threshold;
+        protected double delay;
+    }
+    class InstrumentDis : InstrumentBase
+    {
+        public InstrumentDis(string insName, InstrumentConfig config) : base(insName, config)
+        {
+
+        }
+        public override void Play(double confidence)
+        {
+            if (confidence > threshold)
+            {
+                if ((DateTime.Now - base.lastDetection).TotalMilliseconds > 1000)
+                {
+                    base.lastDetection = DateTime.Now;
+                    sp.Play();
+                }
+            }
+        }
+    }
+    class InstrumentCon : InstrumentBase
+    {
+        public InstrumentCon(string insName, InstrumentConfig config) : base(insName, config)
+        {
+            isRinging = false;
+        }
+        public override void Play(double confidence)
+        {
+            if (confidence > threshold)
+            {
+                lastDetection = DateTime.Now;
+                if (!isRinging)
+                {
+                    sp.PlayLooping();
+                    isRinging = true;
+                }
+            }
+            else
+            {
+                if (isRinging)
+                {
+                    if ((DateTime.Now - lastDetection).TotalMilliseconds > delay)
+                    {
+                        isRinging = false;
+                        sp.Stop();
+                    }
+
+                }
+            }
+        }
+        private bool isRinging;
+    }
+    public class InstrumentConfig
+    {
+        public double threshold;
+        public double delay;
+        /// <summary>
+        /// is discrete?
+        /// </summary>
+        public bool isDiscrete;
+    }
+}
